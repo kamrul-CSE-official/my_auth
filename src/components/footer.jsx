cbbb6611bafb639e7de97463a17e13811487d757
kamrul-CSE-official <kamrulhasan635653@gmail.com>
2024-06-17 00:43:03 +0600

    update

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,23 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
     <div style={{ margin: "100px", border: "1px solid", padding: "10px" }}>
-      This is footer. @ {year}
+      This is footer. <br />
+      <div
+        style={{
+          listStyle: "none",
+          fontWeight: "bold",
+          display: "flex",
+          gap: "10px",
+        }}
+      >
+        <Link style={{ color: "orange" }} to="/">
+          MY AUTH
+        </Link>
+        @ {year}
+      </div>
     </div>
   );
 };
