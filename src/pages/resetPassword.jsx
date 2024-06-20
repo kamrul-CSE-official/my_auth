8e1c19e8ec983e03c18d15aeea38727819e9dda7
kamrul-CSE-official <kamrulhasan635653@gmail.com>
2024-06-21 02:10:27 +0600

    update

--- src/pages/resetPassword.jsx
+++ src/pages/resetPassword.jsx
@@ -1,8 +1,14 @@
+import { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 
 const ResetPasswordPage = () => {
   const [searchParams] = useSearchParams();
   const token = searchParams.get("token");
+
+  useEffect(() => {
+    if (!token) window.location.href = "/login";
+  }, []);
+
   return (
     <div>
       Reset password
